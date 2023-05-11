8e04530e00c472d72b12effb8bca7657b39e6b41
Taís Sousa <nascimentotaais@gmail.com>
2023-05-11 16:23:45 -0300

    2

--- datavis/dashboard/admin.py
+++ datavis/dashboard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import dados
+
+# Register your models here.
+admin.site.register(dados)
+
 # Register your models here.
--- datavis/dashboard/views.py
+++ datavis/dashboard/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
+from .models import dados
 
 # Create your views here.
 
-dt =[
-          7000,
-          21345,
-          18483,
-          24003,
-          23489,
-          24092,
-          26034
-  ]
+# dt =[
+#           7000,
+#           21345,
+#           18483,
+#           24003,
+#           23489,
+#           24092,
+#           26034
+#   ]
 
 def index(request):
+    dt = []
+    values = dados.objects.all()
+    for v in values:
+        dt.append(v.value)
     return render(request, 'index.html', {"dt2": dt})
+
+
+# def index(request):
+#     return render(request, 'index.html', {"dt2": dt})
